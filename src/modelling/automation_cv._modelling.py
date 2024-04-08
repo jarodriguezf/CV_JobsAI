e8c56c8637a8c57b0d269d674a0263195ce6d251
jarodriguezf <jarodriguezf123@gmail.com>
2024-04-08 13:30:44 +0200

    script-automation_cv_modelling

---
+++ src/modelling/automation_cv._modelling.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+from transformers import AutoTokenizer, TFAutoModel
+from sklearn.metrics.pairwise import cosine_similarity
+
+# Carga del modelo y tokenizador
+model_name = 'sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2'
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = TFAutoModel.from_pretrained(model_name)
+
+# Eliminamos salto caracter \n
+def delete_line_break(df):
+    df_copy = df.copy()
+    df_copy.iloc[:, 1] = df_copy.iloc[:, 1].apply(lambda x: x.replace('\n', ' '))
+    return df_copy
+
+# Iniciamos el proceso de carga y extraccion de los embeddings (correspondientes a todos los CV que tengamos)
+def run_automation_modelling_cv(data_cv):
+    # Eliminamos caracter '\n' salto de linea
+    df_cvs=delete_line_break(data_cv)
+  
+    # Tokenizamos los textos
+    tokenized_inputs = tokenizer(df_cvs['cv'].to_list(), padding=True, 
+                                truncation=True, max_length=768, return_tensors="tf")
+    
+    # Extraemos el resultado en funcion de las representaciones de MiniLM
+    outputs = model(tokenized_inputs)
+
+    # Capturamos los embeddings
+    embeddings = outputs.last_hidden_state
+
+    # Normaliza los vectores de embedding para asegurarte de que tengan longitud unitaria
+    normalized_embeddings_cv = embeddings / np.linalg.norm(embeddings, axis=1, keepdims=True)
+    return normalized_embeddings_cv, data_cv
