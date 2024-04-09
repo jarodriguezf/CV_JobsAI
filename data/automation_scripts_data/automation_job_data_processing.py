36647c29300a50b7fc431c61b586a7786c449065
jarodriguezf <jarodriguezf123@gmail.com>
2024-04-09 20:01:49 +0200

    desarrollo general automatizacion y estructura de carpetas

--- data/automation_scripts_data/automation_job_data_processing.py
+++ data/automation_scripts_data/automation_job_data_processing.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
 import re
-import string
-import ast
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
@@ -10,25 +8,25 @@ from nltk.stem import WordNetLemmatizer
 stop_words = set(stopwords.words('spanish'))
 lemmatizer = WordNetLemmatizer()
 
-# Division de categorias CV (software, network, IA)
-def division_cvs(text):
-    text_div =text.split('CV ')
+# Division de categorias ofertas (software, network, IA)
+def division_jobs(text):
+    text_div =text.split('Jobs ')
     text_div=text_div[1:]# Elimina fila vacia
     return  text_div
 
 # Fusionar categorias en un solo vector
-def merge_cvs(first_text, second_text, third_text):
+def merge_jobs(first_text, second_text, third_text):
     merged_texts = []
     
-    # Agregar a lista nueva los datos de los trees tipos de cv
+    # Agregar a lista nueva los datos de los trees tipos de oferta
     merged_texts.extend(first_text)
     merged_texts.extend(second_text)
     merged_texts.extend(third_text)
 
     return merged_texts   
 
-# Borramos numeros correspondiente a cada cv ej. 1: Desarrollador,2: Desarrollador, etc.
-def delete_numbersCV (texts):
+# Borramos numeros correspondiente a cada job ej. 1: Desarrollador,2: Ingeniero, etc.
+def delete_numbersJobs (texts):
     new_texts = []
     for text in texts:
         # Revisar si primer caracter es un digito
@@ -47,63 +45,61 @@ def delete_numbersCV (texts):
             new_texts.append(text)
     return new_texts
 
-# Agregar ID a cada registro (cv)
+# Agregar ID a cada registro (puesto o job)
 def set_id(texts):
     new_texts = [(id_, text) for id_, text in enumerate(texts)]
     return new_texts
 
-# Limpiamos previamente el texto (signos de puntuacion y conversion a minusculas)
 def clean_texts(df):
     df_copy = df.copy()
     words_split = []
     # Separamos en palabras eliminando caracteres especiales
-    for text in df_copy['cv']:
+    for text in df_copy['job']:
         words = re.split(r'\W+', text)
         words = [word.lower() for word in words]
         words_split.append(words)
     # Eliminar ultimo elemento de words_split (es un elemento vacio)
     words_split = [words[:-1] for words in words_split]
-    # Eliminar columna cv y añadir nueva columna con tokens limpios
-    df_copy.drop('cv', axis=1, inplace=True)
-    df_copy['cv_tokens'] = words_split
+    # Eliminar columna job y añadir nueva columna con tokens limpios
+    df_copy.drop('job', axis=1, inplace=True)
+    df_copy['job_tokens'] = words_split
 
     return df_copy
 
-# Limpieza semantica de textos
 def clean_data(df):
     df_copy = df.copy()
     # Convertir las cadenas de texto a lista de token real (pasamos de ["[...]"] a [[]])
-    df_copy['cv_tokens'] = df_copy['cv_tokens'].apply(lambda x: ast.literal_eval(x))
+    #df_copy['job_tokens'] = df_copy['job_tokens'].apply(lambda x: ast.literal_eval(x))
     # Stopwords
-    df_copy['cv_tokens'] = df_copy['cv_tokens'].apply(lambda tokens: [token for token in tokens if token not in stop_words])
+    df_copy['job_tokens'] = df_copy['job_tokens'].apply(lambda tokens: [token for token in tokens if token not in stop_words])
     # Lematizacion de sustantivos
-    df_copy['cv_tokens'] = df_copy['cv_tokens'].apply(lambda tokens: [lemmatizer.lemmatize(token, pos='n') for token in tokens])
+    df_copy['job_tokens'] = df_copy['job_tokens'].apply(lambda tokens: [lemmatizer.lemmatize(token, pos='n') for token in tokens])
     # Lematizacion de verbos
-    df_copy['cv_tokens'] = df_copy['cv_tokens'].apply(lambda tokens: [lemmatizer.lemmatize(token, pos='v') for token in tokens])
+    df_copy['job_tokens'] = df_copy['job_tokens'].apply(lambda tokens: [lemmatizer.lemmatize(token, pos='v') for token in tokens])
     return df_copy
 
 # Unimos tokens separados por espacios ' '
 def join_tokens(df):
     df_copy = df.copy()
 
-    df_copy['cv_tokens'] = df_copy['cv_tokens'].apply(lambda tokens: ' '.join(tokens))
+    df_copy['job_tokens'] = df_copy['job_tokens'].apply(lambda tokens: ' '.join(tokens))
     return df_copy
 
 
-def run_processing_data_cv(data_cv_software, data_cv_network, data_cv_IA):
+def run_processing_data_job(data_job_software, data_job_network, data_job_IA):
     print('--INICIANDO PROCESAMIENTO--')
-    print('Analizando CVs y extraccion de categorias...')
-    div_cv_SoftwareDev = division_cvs(data_cv_software)
-    div_cv_NetworkEng= division_cvs(data_cv_network)
-    div_cv_IADev=division_cvs(data_cv_IA)
+    print('Analizando Jobs y extraccion de categorias...')
+    div_job_SoftwareDev = division_jobs(data_job_software)
+    div_job_NetworkEng= division_jobs(data_job_network)
+    div_job_IADev=division_jobs(data_job_IA)
     print('Fusionando categorias procesadas...')
-    merged_texts = merge_cvs(div_cv_SoftwareDev, div_cv_NetworkEng, div_cv_IADev)
+    merged_texts = merge_jobs(div_job_SoftwareDev, div_job_NetworkEng, div_job_IADev)
     print('Eliminando valores innecesarios...')
-    merged_texts= delete_numbersCV(merged_texts)
+    merged_texts= delete_numbersJobs(merged_texts)
     print('Estableciendo id de posicion...')
     texts_ = set_id(merged_texts)
     print('Convirtiendo a Dataframe...')
-    df_data = pd.DataFrame(texts_, columns=['id', 'cv'])
+    df_data = pd.DataFrame(texts_, columns=['id', 'job'])
     print('Limpieza inicial de datos...')
     df_clean = clean_texts(df_data)
     print('Limpieza semantica de datos...')
@@ -112,4 +108,3 @@ def run_processing_data_cv(data_cv_software, data_cv_network, data_cv_IA):
     df = join_tokens(df)
     print('--FIN DEL PROCESAMIENTO--')
     return df
-
--- src/modelling/automation_scripts_model/automation_cv_modelling.py
+++ src/modelling/automation_scripts_model/automation_cv_modelling.py
@@ -6,13 +6,13 @@ from utils import carga_modelo_tokenizer, delete_line_break
 model_name = 'sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2'
 tokenizer, model = carga_modelo_tokenizer(model_name)
 
-# Iniciamos el proceso de carga y extraccion de los embeddings (correspondientes a todos las ofertas que tengamos)
-def run_automation_modelling_job(data_job):
+# Iniciamos el proceso de carga y extraccion de los embeddings (correspondientes a todos los CV que tengamos)
+def run_automation_modelling_cv(data_cv):
     # Eliminamos caracter '\n' salto de linea
-    df_job=delete_line_break(data_job)
+    df_cvs=delete_line_break(data_cv)
   
     # Tokenizamos los textos
-    tokenized_inputs = tokenizer(df_job['job'].to_list(), padding=True, 
+    tokenized_inputs = tokenizer(df_cvs['cv_tokens'].to_list(), padding=True, 
                                 truncation=True, max_length=768, return_tensors="tf")
     
     # Extraemos el resultado en funcion de las representaciones de MiniLM
@@ -22,5 +22,5 @@ def run_automation_modelling_job(data_job):
     embeddings = outputs.last_hidden_state
 
     # Normaliza los vectores de embedding para asegurarte de que tengan longitud unitaria
-    normalized_embeddings_job = embeddings / np.linalg.norm(embeddings, axis=1, keepdims=True)
-    return normalized_embeddings_job, df_job
+    normalized_embeddings_cv = embeddings / np.linalg.norm(embeddings, axis=1, keepdims=True)
+    return normalized_embeddings_cv, df_cvs
