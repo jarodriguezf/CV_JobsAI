59b9cf286e08a01f5e8afaf9649f27b69deae516
jarodriguezf <jarodriguezf123@gmail.com>
2024-04-14 19:13:29 +0200

    modificacion para adaptarlo a la api

--- data/automation_scripts_data/automation_data_save.py
+++ data/automation_scripts_data/automation_data_save.py
@@ -1,33 +1,12 @@
 import re
 
 # Constantes de categorias tanto para CV como ofertas de trabajo, rutas de guardado para CV y ofertas.
-CATEGORIES = ['software', 'ia', 'network']
 FILE_PATHS = {
-    'software': '../raw_data/Data_{id_name}_SoftwareDeveloper.txt',
-    'ia': '../raw_data/Data_{id_name}_IADeveloper.txt',
-    'network': '../raw_data/Data_{id_name}_NetworkEngineer.txt'
+    'software': '../../data/raw_data/Data_{id_name}_SoftwareDeveloper.txt',
+    'ia': '../../data/raw_data/Data_{id_name}_IADeveloper.txt',
+    'network': '../../data/raw_data/Data_{id_name}_NetworkEngineer.txt'
 }
-
-# Validamos si la opcion elegida es la correcta
-def validate_input(prompt, options):
-    while True:
-        print(prompt) # Mostramos el menu por pantalla
-        option = input().lower()
-        if option not in options:
-            print(f'Error. Solo puedes especificar la opción como {", ".join(options)}.')
-        else:
-            return option
-        
-# Pintamos el menu de opciones (software, ia, network)
-def options_menu(id_name):
-    if not isinstance(id_name, str):
-        print('Error. Solo se puede introducir cadenas de texto')
-    else:    
-        option_cv_job = id_name.lower()
-        prompt = f'¿Qué tipo de {id_name} deseas guardar? (software, ia, network)'
-        option_categories = validate_input(prompt, CATEGORIES) # funcion para validar categoria elegida
-        return option_cv_job, option_categories
-    
+ 
 # Recorremos cada txt correspondiente a la ruta seleccionada y devolvemos id ultimo registro.
 def insert_position_txt(filename, id_name):
     try:
@@ -46,11 +25,12 @@ def insert_position_txt(filename, id_name):
         return None
     
 # Guarda los datos en funcion de si es una oferta o un cv. Además de la categoria de cada uno de ellos.
-def save_data(data, id_name):
+def save_data(data, id_name, categories):
     if not isinstance(id_name, str) or not isinstance(data, str):
         print('Error. Solo se puede introducir cadenas de texto')
     else:
-        option_cv_job, option_categories = options_menu(id_name)# funcion de opciones menu
+        option_cv_job = id_name.lower()
+        option_categories = categories.lower()
 
         # Comprobamos si el nombre de la oferta o cv es igual al pasado por parametros.
         # Comprobamos si la clave del FILE_PATHS es igual a las categorias elegidas en las opciones del menu
@@ -71,5 +51,5 @@ def save_data(data, id_name):
         else:
             print(f"Opción de {id_name} o categoría no válida.")
 
-def run_automation_save_data(data, type):
-    save_data(data, type) # Guarda Curriculums o ofertas de trabajo (CV/Jobs)
+def run_automation_save_data(data, type, categories):
+    save_data(data, type, categories) # Guarda Curriculums o ofertas de trabajo (CV/Jobs)
