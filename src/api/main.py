25b1d1a1b90908d284f6977a56549750de152a4a
jarodriguezf <jarodriguezf123@gmail.com>
2024-04-15 11:56:32 +0200

    desarrollo endpoints new template

--- src/api/main.py
+++ src/api/main.py
@@ -11,8 +11,10 @@ app = FastAPI()
 
 # Archivos estaticos (front)
 app.mount("/static", StaticFiles(directory="static/landing_page"), name="static")
+
 templates_landing = Jinja2Templates(directory="static/landing_page")
 insert_data_template = Jinja2Templates(directory="static/insert-data-pages")
+mostrar_cribar = Jinja2Templates(directory="static/show-similarities-page")
 
 # Carga de la landing page
 @app.get("/")
@@ -20,10 +22,15 @@ async def root(request: Request):
     return templates_landing.TemplateResponse("index.html", {"request": request})
 
 
-# Carga de pagina home_control (a traves de "index.html")
+# Carga de pagina insercion de datos (a traves de "index.html")
 @app.get("/insert-data")
 async def home(request: Request):
     return insert_data_template.TemplateResponse("control_index.html", {"request": request})
+
+# Carga de pagina mostrar informacion y criba (a traves de "control_index.html")
+@app.get("/show-similarities")
+async def home(request: Request):
+    return mostrar_cribar.TemplateResponse("similarities.html", {"request": request})
 
 
 # Endpoint para el botón de guardar CV
